3aba3bd937e3daa57bb0529eb2dae32ed7f2fd4c
Darrell Vinson <darrellv@gmail.com>
2018-02-07 14:34:37 -0500

    First Commit
    Committing new complete application

---
+++ HackerNewsTitleAndAuthor/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="HackerNewsTitleAndAuthor.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="grdStoryList" HeaderStyle-BackColor="#3AC0F2" HeaderStyle-ForeColor="White" runat="server" AutoGenerateColumns="false">
+                <Columns>
+                    <asp:BoundField DataField="title" HeaderText="Title" ItemStyle-Width="500" />
+                    <asp:BoundField DataField="by" HeaderText="Author" ItemStyle-Width="200" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ HackerNewsTitleAndAuthor/Default.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using HackerNewsTitleAndAuthor.Classes;
+
+namespace HackerNewsTitleAndAuthor
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Get list of top story IDs
+            JArray topStories = JArray.Parse(CallService(" https://hacker-news.firebaseio.com/v0/topstories.json"));
+
+            //convert IDs to a list of integers
+            List<int> storyIDs = topStories.ToObject<List<int>>();
+
+            List<Story> storyList = new List<Story>();
+
+            //add each story to the list of stories
+            foreach (int ID in storyIDs)
+            {
+                Story storyObject = JsonConvert.DeserializeObject<Story>(CallService(string.Format("https://hacker-news.firebaseio.com/v0/item/{0}.json", ID)));
+                storyList.Add(storyObject);
+            }
+
+            //display the story titles and authors in a datagrid
+            grdStoryList.DataSource = storyList;
+            grdStoryList.DataBind();
+       }
+
+        protected string CallService(string url)
+        {
+            RestClient client = new RestClient(url);
+            IRestResponse response = client.Execute(new RestRequest());
+            return response.Content;
+
+        }
+
+    }
+}
